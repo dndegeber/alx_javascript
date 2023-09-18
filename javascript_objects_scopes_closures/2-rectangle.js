695bf1be42ed194bbcb3b7d4e53353e4b73598b6
dndegeber <dorandege@gmail.com>
2023-09-18 14:34:05 +0300

    Rectangle 2

---
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,13 @@
+class Rectangle {
+    constructor(w, h) {
+      if (w > 0 && h > 0) {
+        this.width = w;
+        this.height = h;
+      } else {
+        // Create an empty object if w or h is not a positive integer
+      }
+    }
+  }
+  
+  module.exports = Rectangle;
+  
