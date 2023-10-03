21b1ba25d78f5433cfefd33bf9e538d467d736cf
dndegeber <dorandege@gmail.com>
2023-10-03 11:49:19 +0300

    Lexical Scoping and Welcome Message

---
+++ Javascript_advanced/0-welcome.js
@@ -0,0 +1,13 @@
+function welcome(firstName, lastName) {
+    var fullName = firstName + ' ' + lastName;
+  
+    function displayFullName() {
+      alert('Welcome ' + fullName + '!');
+    }
+  
+    displayFullName();
+  }
+  
+  // Test the function
+  welcome('Holberton', 'School');
+  
