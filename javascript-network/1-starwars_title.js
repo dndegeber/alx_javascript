b7b93bbcf4d06dcd6b1ca5408a03d502290ecc91
dndegeber <dorandege@gmail.com>
2023-10-04 00:11:36 +0300

    Star wars movie

---
+++ javascript-network/1-starwars_title.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const movieID = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieID}`;
+
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+    return;
+  }
+
+  const movieData = JSON.parse(body);
+  console.log(movieData.title);
+});
