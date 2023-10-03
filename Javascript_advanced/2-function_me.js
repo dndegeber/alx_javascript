d1d4e9901c69ca1bb7a0d26a25ca459c45a9a012
dndegeber <dorandege@gmail.com>
2023-10-03 12:17:46 +0300

    Closure

---
+++ Javascript_advanced/2-function_me.js
@@ -0,0 +1,15 @@
+function welcomeMessage(fullName) {
+    return function() {
+      alert('Welcome ' + fullName);
+    }
+  }
+  
+  var guillaume = welcomeMessage('Guillaume');
+  var alex = welcomeMessage('Alex');
+  var fred = welcomeMessage('Fred');
+  
+  // Testing the functions
+  guillaume();
+  alex();
+  fred();
+  
