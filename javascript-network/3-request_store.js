e87baf37db823dda47626b4379a0e88e73dfe8cd
dndegeber <dorandege@gmail.com>
2023-10-28 00:03:23 +0300

    Loripsum correction

--- javascript-network/3-request_store.js
+++ javascript-network/3-request_store.js
@@ -1,22 +1,34 @@
+
 #!/usr/bin/node
 
+// Import the required modules
 const request = require('request');
 const fs = require('fs');
 
+// Get the URL and file path from the command line arguments
 const url = process.argv[2];
 const filePath = process.argv[3];
 
-request.get(url, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
-    return;
-  }
+// Check if both URL and file path are provided as arguments
+if (!url || !filePath) {
+  console.error('Please provide both a URL and a file path as arguments.');
+  process.exit(1);
+}
 
-  fs.writeFile(filePath, body, 'utf8', (err) => {
-    if (err) {
-      console.error('Error writing file:', err);
-      return;
-    }
-    console.log('File saved successfully!');
-  });
+// Send a GET request to the specified URL
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(`Error: ${error.message}`);
+  } else if (response.statusCode !== 200) {
+    console.error(`Error: Status code ${response.statusCode}`);
+  } else {
+    // Write the response body to the specified file with UTF-8 encoding
+    fs.writeFile(filePath, body, 'utf-8', (writeError) => {
+      if (writeError) {
+        console.error(`Error writing to file: ${writeError.message}`);
+      } else {
+        console.log(`Contents from ${url} saved to ${filePath}`);
+      }
+    });
+  }
 });
